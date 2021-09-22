0cf84298919d13433ae16ddfd4502d858293f077
Mike11199 <miwanek11199@gmail.com>
2021-09-21 19:51:51 -0700

    added variables

--- week1/python-2.py
+++ week1/python-2.py
@@ -5,3 +5,9 @@ print(msg2)
 print(msg2)
   
 # a variable is a container for a value.  Behaves as the value that it contains.
+
+first_name = "Jeb"
+last_name = "Kerman"
+full_name = first_name + " " + last_name
+print("Hello " + full_name)
+print(type(name))
