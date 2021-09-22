07205797bbeabcfeb203910e35fb6397671af00e
Mike11199 <miwanek11199@gmail.com>
2021-09-21 20:32:47 -0700

    Changed the file names around to be more descriptive.

---
+++ week1/python-2-multiple assignment.py
@@ -0,0 +1,17 @@
+# multiple assignment = allows us to assign multiple variables at the same time in one line of code
+
+# name = "Jeb"
+# age = 21
+# astronaut = True
+
+name, age, astronaut ="Jeb", 21, True
+
+print(name)
+print(age)
+print(astronaut)
+
+Spongebob = Patrick = Sandy = Squidward = 30
+
+print(Spongebob)
+print(Patrick)
+print(Sandy)
