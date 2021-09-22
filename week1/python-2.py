9bdfce1fa169a320f5cbd46bea1c21b2e2cb26f1
Mike11199 <miwanek11199@gmail.com>
2021-09-21 20:16:24 -0700

    added variables

--- week1/python-2.py
+++ week1/python-2.py
@@ -11,3 +11,15 @@ last_name = "Kerman"
 full_name = first_name + " " + last_name
 print("Hello " + full_name)
 print(type(full_name))
+
+# age =21
+# age +=1
+# print("Your age is " + str(age))
+
+height = 250.5
+print("Your height is : " +str(height)+"cm")
+#print(type(height))
+
+human = True
+#print(type(human))
+print("Are you a human:  "+str(human))
