c39f901bece692c5701a9565a4573c0df5a6782f
Mike11199 <miwanek11199@gmail.com>
2021-09-21 21:07:17 -0700

    Edited file.

--- week1/python-4-typecast.py
+++ week1/python-4-typecast.py
@@ -1,2 +1,14 @@
 #Type casting = convert the data type fo a value to another data type
 
+x = 1 #int
+y = 2.0 #float
+z = "3" #str
+
+x = str(x)
+y = str(y)
+z = float(z)
+
+
+print("X is " + x)  #use type casting to convert to string so can print with a string statement
+print("Y is " + y)
+print(z*3)
