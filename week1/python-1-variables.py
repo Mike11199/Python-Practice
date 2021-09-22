282bfdbc991f1182a94e3f2912eeec24aebb5ddf
Mike11199 <miwanek11199@gmail.com>
2021-09-21 20:33:44 -0700

    Changed the file names around to be more descriptive.

---
+++ week1/python-1-variables.py
@@ -0,0 +1,25 @@
+msg = "Hello World"
+print(msg)
+msg2 = "testing pushing to git from pycharm"
+print(msg2)
+print(msg2)
+  
+# a variable is a container for a value.  Behaves as the value that it contains.
+
+first_name = "Jeb"
+last_name = "Kerman"
+full_name = first_name + " " + last_name
+print("Hello " + full_name)
+print(type(full_name))
+
+# age =21
+# age +=1
+# print("Your age is " + str(age))
+
+height = 250.5
+print("Your height is : " +str(height)+"cm")
+#print(type(height))
+
+human = True
+#print(type(human))
+print("Are you a human:  "+str(human))
