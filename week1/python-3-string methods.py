91030bf76e3d3d6e91f1b380d9dfbc46df90b632
Mike11199 <miwanek11199@gmail.com>
2021-09-21 20:56:27 -0700

    Added file.

---
+++ week1/python-3-string methods.py
@@ -0,0 +1,14 @@
+#string methods available
+
+name = "jeb"
+
+print(len(name))
+print(name.find("e"))
+print(name.capitalize())
+print(name.lower())
+print(name.upper())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("j"))
+print(name.replace("j","l"))
+print(name*3)
