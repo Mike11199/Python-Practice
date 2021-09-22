7d8e56aaf2ffd4a868a6ce8c5c806e1aec7ae169
Mike11199 <miwanek11199@gmail.com>
2021-09-21 22:49:45 -0700

    Edited file.

---
+++ week1/python-10-while loops.py
@@ -0,0 +1,11 @@
+# while loop = a statement that will execute its block of code, as long as it's condition remains true
+
+# while 1==1:
+#     print("Help!  I'm stuck in a loop!")
+
+name = None
+
+while not name:
+    name = input("Enter your name: ")
+
+print("Hello "+name)
