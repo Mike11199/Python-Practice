a89baad396f629952ca63c47f1122cf621f63183
Mike11199 <miwanek11199@gmail.com>
2021-09-23 21:49:43 -0700

    add

--- week1/xlwings-test.py
+++ week1/xlwings-test.py
@@ -1,8 +1,18 @@
 import xlwings as xw
+import pandas as pd
 
-wk = xw.books.open(r"C:\Users\miwan\Desktop\Everest Times.xlsx")
+# xw.App().visible = False         #this should be used if you want to not have the excel file open
+
+wk = xw.books.open(r'C:\Users\miwan\Desktop\Everest Times.xlsx')
 # sheet = wk.sheets["Sheet1"]
 sheet = wk.sheets[0]
 rg = sheet.range("A1:G20")
 print(rg.value)
 
+
+df = sheet.range("A1:G20").options(pd.DataFrame).value     #creates dataframe
+
+
+xw.view(df)                 #opens a new workbook in Excel to view dataframe result
+
+# xw.close
