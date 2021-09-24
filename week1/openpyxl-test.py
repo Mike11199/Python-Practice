bf120c9e679d88203a117f3a88021a920782a9d5
Mike11199 <miwanek11199@gmail.com>
2021-09-23 21:22:03 -0700

    e

--- week1/openpyxl-test.py
+++ week1/openpyxl-test.py
@@ -6,7 +6,6 @@ book = openpyxl.load_workbook(r"C:\Users\miwan\Desktop\Everest Times.xlsx")
 
 sheet = book["Sheet1"]
 
-# print(sheet["A8"])
 print(sheet["C6"].value)
 print(sheet.cell(3,4).value)
 
