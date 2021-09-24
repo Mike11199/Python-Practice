66c62ff98c2dd64b0881b7be1dd2a1f038d401db
Mike11199 <miwanek11199@gmail.com>
2021-09-23 21:21:13 -0700

    edit

--- week1/openpyxl-test.py
+++ week1/openpyxl-test.py
@@ -1,4 +1,6 @@
+from re import M
 import openpyxl
+
 
 book = openpyxl.load_workbook(r"C:\Users\miwan\Desktop\Everest Times.xlsx")
 
@@ -8,3 +10,9 @@ sheet = book["Sheet1"]
 print(sheet["C6"].value)
 print(sheet.cell(3,4).value)
 
+sheet["B23"] = "Hello There"
+
+book.save(r"C:\Users\miwan\Desktop\Everest Times.xlsx")
+
+for row in sheet.iter_rows(values_only=True):
+    print(row)
