03872bb733f940a46a86e607235e5309727567f3
Mike11199 <miwanek11199@gmail.com>
2021-09-29 21:47:16 -0700

    add

---
+++ week1/Valid_Parentheses.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        pair = {"(": ")", "[": "]", "{": "}"}
+        stack = []
+        for b in s:
+            if not stack:
+                if b not in pair:
+                    return False
+                stack.append(b)
+            else:
+                if b not in pair and pair[stack[-1]] != b:
+                    return False
+                if b == pair[stack[-1]]:
+                    stack.pop()
+                else:
+                    stack.append(b)
+        return not stack
+
+
+test = Solution()
+print(test.isValid("[](){}"))
