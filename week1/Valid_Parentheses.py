246dd4629d707dc1c0fca41e4d7e8f3d20521b66
Mike11199 <miwanek11199@gmail.com>
2021-10-01 19:15:38 -0700

    edit

--- week1/Valid_Parentheses.py
+++ week1/Valid_Parentheses.py
@@ -4,22 +4,25 @@ class Solution:
         :type s: str
         :rtype: bool
         """
-        pair = {"(": ")", "[": "]", "{": "}"}
+        pair = {"(": ")", "[": "]", "{": "}"}   #Dictionary; a set of key: value pairs with each being unique.
         stack = []
         for b in s:
-            if not stack:
-                if b not in pair:
+            if not stack:               #tests if the list is empty 
+                if b not in pair:       #if the character is not in dictionary at all, then return false and end
                     return False
                 stack.append(b)
             else:
-                if b not in pair and pair[stack[-1]] != b:
+                if b not in pair and pair[stack[-1]] != b:   
                     return False
                 if b == pair[stack[-1]]:
-                    stack.pop()
+                    print(pair[stack[-1]])
+                    stack.pop()    #deletes topmost element of the stack
+                    
                 else:
-                    stack.append(b)
-        return not stack
+                    stack.append(b)  #
+        return not stack             #if the stack is empty, so it will return false, return true
 
 
 test = Solution()
-print(test.isValid("[](){}"))
+print(test.isValid("[(){}]"))
+
